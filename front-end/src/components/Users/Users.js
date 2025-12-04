12871f3b8df89dde79c433f6c6a6a3104709a25e
AliShakeri83 <ali.sh87873@gmail.com>
2025-12-05 01:09:58 +0330

    handle delete modal in users component

--- front-end/src/components/Users/Users.js
+++ front-end/src/components/Users/Users.js
@@ -1,9 +1,23 @@
 import React, { useState } from "react";
 import Errorbox from "../Errorbox/Errorbox";
 import { allUsersInShop } from "../../Datas";
+import DeleteModal from "./../DeleteModal/DeleteModal";
 
 export default function Users() {
   const [users, setUsers] = useState(allUsersInShop);
+  const [isShowDeleteModal, setIsShowDeleteModal] = useState(false);
+  const [mainUserID, setMainUserID] = useState(null);
+  const closeDeleteModal = () => {
+    setIsShowDeleteModal(false);
+  };
+  const deleteUser = () => {
+    console.log("delete user");
+    const newUserArray = users.filter((user) => {
+      return user.id !== mainUserID;
+    });
+    setUsers(newUserArray);
+    setIsShowDeleteModal(false);
+  };
   return (
     <>
       <div className="cms-main">
@@ -31,7 +45,15 @@ export default function Users() {
                   <td>{user.phone}</td>
                   <td>{user.email}</td>
                   <td>
-                    <button className="btn">حذف</button>
+                    <button
+                      className="btn"
+                      onClick={() => {
+                        setIsShowDeleteModal(true);
+                        setMainUserID(user.id);
+                      }}
+                    >
+                      حذف
+                    </button>
                     <button className="btn">جزئیات</button>
                     <button className="btn">ویرایش</button>
                   </td>
@@ -41,6 +63,13 @@ export default function Users() {
           </table>
         ) : (
           <Errorbox msg="هیچ کاربری یافت نشد" />
+        )}
+        {isShowDeleteModal && (
+          <DeleteModal
+            title={"آیا از حذف اطمینان دارید؟"}
+            deleteModalCancelAction={closeDeleteModal}
+            deleteModalSubmitAction={deleteUser}
+          />
         )}
       </div>
     </>
