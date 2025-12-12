e850b51c3f9d65b6a966550dcdfa32942d9eb658
AlirezaSaadatmand <alirezasaadat056@gmail.com>
2025-12-12 14:22:06 +0330

    add(types/index.ts): adding new types for new models

--- back-end/src/types/index.ts
+++ back-end/src/types/index.ts
@@ -20,3 +20,66 @@ export interface IProduct {
   createdAt?: Date;
   updatedAt?: Date;
 }
+
+export interface IUser {
+  id?: number;
+  firstname: string;
+  lastname: string;
+  username: string;
+  password: number;
+  phone?: string;
+  city?: string;
+  email: string;
+  address?: string;
+  score?: bigint;
+  buy?: bigint;
+
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export interface IComment {
+  id?: number;
+  body: string;
+  date: string;
+  hour: string;
+  userID: number;
+  username: string;
+  productID: number;
+  productName: string;
+  isAction?: boolean;
+
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export interface IOrder {
+  id?: number;
+  productID: number;
+  productName: string;
+  userID: number;
+  username: string;
+  date: string;
+  hour: string;
+  price: bigint;
+  off?: number;
+  sale?: number;
+  saleCount?: bigint;
+  isActive?: boolean;
+
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export interface IDiscount {
+  id?: number;
+  code: string;
+  precent: number;
+  productID: number;
+  productName: string;
+  date: string;
+  isActive?: boolean;
+
+  createdAt?: Date;
+  updatedAt?: Date;
+}
