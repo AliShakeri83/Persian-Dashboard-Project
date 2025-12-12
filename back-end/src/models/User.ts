4ba26cdae38a9656f6355990496ebc6a923ac86c
AlirezaSaadatmand <alirezasaadat056@gmail.com>
2025-12-12 14:23:38 +0330

    add(User.ts): User model

---
+++ back-end/src/models/User.ts
@@ -0,0 +1,79 @@
+import { DataTypes, Model, Optional } from "sequelize";
+import { sequelize } from "../database/database";
+import { IUser } from "../types";
+
+interface UserCreationAttributes
+  extends Optional<IUser, "id" | "createdAt" | "updatedAt"> {}
+
+export class Users extends Model<IUser, UserCreationAttributes>
+  implements IUser {
+  public id!: number;
+  public firstname!: string;
+  public lastname!: string;
+  public username!: string;
+  public password!: number;
+  public phone!: string;
+  public city!: string;
+  public email!: string;
+  public address!: string;
+  public score!: bigint;
+  public buy!: bigint;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+Users.init(
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    firstname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    score: {
+      type: DataTypes.BIGINT,
+      defaultValue: 0,
+    },
+    buy: {
+      type: DataTypes.BIGINT,
+      defaultValue: 0,
+    },
+  },
+  {
+    sequelize,
+    modelName: "Users",
+  }
+);
